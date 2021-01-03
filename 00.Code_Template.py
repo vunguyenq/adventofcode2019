dd5d37056ec232cc112291d4b2ef38ba3f923f7e
Vu Nguyen <vu.nguyenq89@gmail.com>
2021-01-03 23:32:09 +0100

    code template created

---
+++ 00.Code_Template.py
@@ -0,0 +1,27 @@
+exec_part = 1 # which part to execute
+exec_test_case = 1 # 1 = test input; 0 = real puzzle input
+INPUT_TEST = '''12'''
+INPUT = '''
+'''
+def parse_input(input):
+    return input.split('\n')
+
+def part1():
+    result = 0
+    return result
+
+def part2():
+    result = 0
+    return result
+
+if __name__ == "__main__":
+    if(exec_test_case == 1):
+        input = INPUT_TEST
+    else:
+        input = INPUT
+
+    if (exec_part == 1):
+        result = part1()
+    else:
+        result = part2()
+    print('Part {} answer: {}'.format(exec_part, result))
